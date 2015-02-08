4ca88a278f23be5a688c912fc5c32bfebdcdfc4c
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-07 22:44:24 -0600

    initial test passed

---
+++ tests/spec/main.test.js
@@ -0,0 +1,8 @@
+/* jshint mocha: true, expr: true, strict: false */
+
+describe('test suite', function() {
+  it('should assert true', function() {
+    true.should.be.true;
+    false.should.be.false;
+  });
+});
